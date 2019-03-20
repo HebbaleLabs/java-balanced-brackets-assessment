45937e6d9437cb29e754f0cc9fb47e277ccf3058
hbhargav <harsha.bhargav@hebbalelabs.com>
2019-03-20 22:47:29 +0530

    Updated Test Case to be Parametrized

--- src/test/java/com/talfinder/assessment/BalancedBracketsTest.java
+++ src/test/java/com/talfinder/assessment/BalancedBracketsTest.java
@@ -1,78 +1,41 @@
 package com.talfinder.assessment;
 
-import com.talfinder.assessment.BalancedBrackets;
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-
+@RunWith(Parameterized.class)
 public class BalancedBracketsTest {
 
-  @Test
-  public void checkBalancedBrackets_simpleEquations() {
+  @Parameters
+  public static Collection<Object[]> parameters() {
+    return Arrays.asList(new Object[][] {
+        {"[2+3(4+5)]", true},
+        {"{1*[2+3(4+5)]}", true},
+        {"{1*2+3)4+5(}", false},
+        {"\"r1*(cosθ1+i*sinθ1)/r2*(cosθ2+i*sinθ2)=r1/r2[cos(θ1−θ2)+i*sin(θ1−θ2)]\"", true},
+        {"{[r1*(cosθ1+i*sinθ1)]*[r2*(cosθ2+i*sinθ2)]=r1/r2[cos(θ1+θ2)+i*sin(θ1+θ2)}]", false},
+        {"3*4+5", true},
+        {"r1*(cosθ1+i*sinθ1)/r2*(cosθ2+i*sinθ2)=r1/r2[cos(θ1−θ2)+i*sin(θ1−θ2)]", true},
+        {"[r*(cosθ+i*sinθ)]*n=r*n(cos(nθ)+i*sin(nθ))", true},
+        {"1", true}
+    });
+  }
 
-    String input = "1*(2+3)";
-    Assert.assertTrue(BalancedBrackets.isBalanced(input));
+  String input;
+  boolean expectedValue;
 
-    input = "[2+3(4+5)]";
-    Assert.assertTrue(BalancedBrackets.isBalanced(input));
-
-    input = "{1*[2+3(4+5)]}";
-    Assert.assertTrue(BalancedBrackets.isBalanced(input));
-
-    input = "1*(2+3";
-    Assert.assertFalse(BalancedBrackets.isBalanced(input));
-
-    input = "1*)2+3";
-    Assert.assertFalse(BalancedBrackets.isBalanced(input));
-
-    input = "{1*2+3)4+5(}";
-    Assert.assertFalse(BalancedBrackets.isBalanced(input));
-
-    input = "{1*2+3(4+5})";
-    Assert.assertFalse(BalancedBrackets.isBalanced(input));
-
-    input = "{1*2+3(4+5)}]";
-    Assert.assertFalse(BalancedBrackets.isBalanced(input));
-
-    input = "}1*2+3(4+5){";
-    Assert.assertFalse(BalancedBrackets.isBalanced(input));
-
-    input = "{1*[2+3(4+5";
-    Assert.assertFalse(BalancedBrackets.isBalanced(input));
+  public BalancedBracketsTest(String input, boolean expectedValue) {
+    this.input = input;
+    this.expectedValue = expectedValue;
   }
 
   @Test
-  public void checkBalancedBrackets_ComplextEquations() {
-
-    String input = "[r*(cosθ+i*sinθ)]*n=r*n(cos(nθ)+i*sin(nθ))";
-    Assert.assertTrue(BalancedBrackets.isBalanced(input));
-
-    input = "r1*(cosθ1+i*sinθ1)/r2*(cosθ2+i*sinθ2)=r1/r2[cos(θ1−θ2)+i*sin(θ1−θ2)]";
-    Assert.assertTrue(BalancedBrackets.isBalanced(input));
-
-    input = "{[r1*(cosθ1+i*sinθ1)]*[r2*(cosθ2+i*sinθ2)]=r1/r2[cos(θ1+θ2)+i*sin(θ1+θ2)]}";
-    Assert.assertTrue(BalancedBrackets.isBalanced(input));
-
-    input = "[r*(cosθ+i*sinθ])*n=r*n(cos(nθ)+i*sin(nθ))";
-    Assert.assertFalse(BalancedBrackets.isBalanced(input));
-
-    input = "r1*(cosθ1+i*sinθ1)/r2*(cosθ2+i*sinθ2)=r1/r2*cos(θ1−θ2)+i*sin(θ1−θ2)]";
-    Assert.assertFalse(BalancedBrackets.isBalanced(input));
-
-    input = "{[r1*(cosθ1+i*sinθ1)]*[r2*(cosθ2+i*sinθ2)]=r1/r2[cos(θ1+θ2)+i*sin(θ1+θ2)}]";
-    Assert.assertFalse(BalancedBrackets.isBalanced(input));
-  }
-
-  @Test
-  public void checkBalancedBrackets_WithoutBrackets() {
-
-    String input = "1+2+3";
-    Assert.assertTrue(BalancedBrackets.isBalanced(input));
-
-    input = "1";
-    Assert.assertTrue(BalancedBrackets.isBalanced(input));
-
-    input = "3*4+5";
-    Assert.assertTrue(BalancedBrackets.isBalanced(input));
+  public void checkBalancedBrackets() {
+    Assert.assertEquals("input:Equation as '"+ input + "'", expectedValue, BalancedBrackets.isBalanced(input));
   }
 }
